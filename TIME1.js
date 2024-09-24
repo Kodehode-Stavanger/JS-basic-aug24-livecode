66917d368b67c6bc016f79c002a3ee504732f317
Endre Jordal <endre@jobloop.no>
2024-09-24 08:22:20 +0200

    fix

---
+++ TIME1.js
@@ -0,0 +1,66 @@
+console.log("Hello World!")
+
+// Filtrerer søkeresultater
+// iuyh
+
+// kode her
+
+/* Multi
+line
+comment
+hei
+på
+deg */
+
+
+//user_age
+// Camel-case
+// isNotAllowed
+let userAge = 20
+userAge = 21
+const userName = "Trond"
+var userEmail = "mail@mail.com"
+const userIsBanned = false
+const darkModeEnabled = false
+console.log(userIsBanned)
+
+// Data typer
+// String
+// Tall
+// Boolean
+// Null, undefined
+// Array
+// Object
+
+const userHobbies = ["Cycling", "TV", "Golf"]
+console.log(userHobbies)
+
+const userData = {
+    userName: "Trond", 
+    userAge: 20, 
+    userHobbies: ["Cycling", "TV", "Golf"], 
+    userIsBanned: false, 
+    userEmail: "mail@mail.com"
+};
+console.log(userData.userEmail)
+
+// Operators
+
+// +, -, *, /
+
+const x = 5
+const y = x * 2
+console.log(y / 4)
+
+let lives = 7
+// lives = lives + 1
+// lives += 2
+// lives++
+lives++
+console.log(lives)
+
+// Comparison operators
+// <, >, ===, >=, <=
+
+const comparison = 4 === 4
+console.log(comparison)
