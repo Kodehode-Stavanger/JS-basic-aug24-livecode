cd906e1ae03efd3d9d8d82a7bcf589b3a4fb9281
Endre Jordal <endre@jobloop.no>
2024-10-01 10:36:24 +0200

    femte time

---
+++ TIME5.js
@@ -0,0 +1,52 @@
+// Methods - innebygde verktøy i Javascript.
+
+// Datatyper har assosierte methods.
+// Bruk W3 Schools og MDN som oppslagsverk.
+
+// Strings
+
+const myString = "Javascript Basic is fun!"
+
+// console.log(myString.replaceAll("Basic", "Advanced"))
+// console.log(myString.split("i"))
+
+// console.log(myString.toUpperCase())
+// console.log(myString.toLowerCase())
+// console.log(myString.at(0))
+// console.log(myString.charCodeAt(1))
+// console.log(myString.slice(-7, -4))
+// substring(), substr()
+// console.log(myString.length)
+//console.log(myString.trim().toUpperCase())
+// console.log(myString.repeat(30))
+
+// Array methods
+
+const fruits = ["Apple", "Banana", "Melon", "Mango", "Pear", "Pineapple", "Cherry"]
+
+
+// console.log(fruits.length)
+// console.log(fruits)
+// console.log(fruits.join("👺").toUpperCase())
+// console.log(fruits[2])
+// fruits.pop()
+// fruits.push("Strawberry")
+// fruits.unshift("Peach")
+// fruits.shift()
+// console.log(fruits.concat(fruits2))
+
+// console.log(fruits.toSpliced(0, 7, "Lemon", "Lime", "Lychee"))
+// console.log(fruits.splice(2,2,"Lemon"))
+
+// console.log(fruits)
+
+// Number methods
+
+const myNumber = 520921587657856476546754
+
+// console.log(myNumber.toFixed(2))
+// console.log(myNumber.toString())
+console.log(myNumber.toExponential())
+
+
+
